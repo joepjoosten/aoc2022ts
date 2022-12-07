d75e9c46a0a5379cdb0b980c3ddf01ee07b2c288
Joep Joosten <joep.joosten@ihomer.nl>
2022-12-07 20:45:00 +0100

    Working day 5

--- src/day5/solution.ts
+++ src/day5/solution.ts
@@ -1,11 +1,10 @@
-import { trace, traceWithValue } from 'fp-ts-std/Debug';
+import { assert } from 'console';
 import { fromString } from 'fp-ts-std/Number';
-import { transpose } from 'fp-ts-std/ReadonlyArray';
+import { join, transpose } from 'fp-ts-std/ReadonlyArray';
 import { log } from 'fp-ts/lib/Console';
-import { flow, pipe } from "fp-ts/lib/function";
-import { fromPredicate, getOrElse, map as omap, compact as ocompact, flatten, some } from 'fp-ts/lib/Option';
-import { compact, concat, dropRight, lookup, modifyAt, reduce, takeLeft, takeRight } from "fp-ts/lib/ReadonlyArray";
-import { chunksOf, map, reverse } from "fp-ts/lib/ReadonlyNonEmptyArray";
+import { flow, pipe, identity } from "fp-ts/lib/function";
+import { chain, fromPredicate, getOrElse, map as omap } from 'fp-ts/lib/Option';
+import { chunksOf, compact, concat, dropRight, flatten, lookup, map, modifyAt, reduce, reverse, takeRight } from "fp-ts/lib/ReadonlyArray";
 import { fst, snd } from 'fp-ts/lib/ReadonlyTuple';
 import * as S from "fp-ts/string";
 import { readFileSync } from "fs";
@@ -21,23 +20,30 @@ const separator = S.split('\n\n');
 const stackLines = flow(separator, fst, lines, dropRight(1));
 const movesLines = flow(separator, snd, lines);
 
-const parseStackLine = flow(chars, chunksOf(4), map(flow(lookup(1), omap(fromPredicate(s => s !== ' ')))), compact);
-const parseMoveLine = flow(words, map(fromString), compact, ([move, from, to]) => [move, from - 1, to - 1]);
+const parseStackLine = flow(chars, chunksOf(4), map(flow(lookup(1), chain(fromPredicate(s => s !== ' ')))));
+const parseMoveLine = flow(words, map(fromString), compact, ([move, from, to]) => [move, from - 1, to - 1] as [number, number, number]);
 
 const parseStack = flow(stackLines, map(parseStackLine), transpose, map(flow(compact, reverse)));
 const parseMoves = flow(movesLines, map(parseMoveLine));
 
-const solve = (stack) => reduce(some(stack), (s, [move, from, to]) => pipe(
-  s, 
-  traceWithValue(`move: ${move}, from: ${from}, to: ${to}`),
-  omap(modifyAt(to, concat(takeRight(move)(pipe(lookup(from)(pipe(s, getOrElse(() => []))), getOrElse(() => [])))))),
-  omap(modifyAt(from, takeLeft(move))),
-  traceWithValue("sss"),
-));
+const solve = (stack, crane: typeof reverse) => flow(
+  reduce(stack, (acc, [move, from, to]) => pipe(
+    acc, 
+    lookup(from), 
+    omap(takeRight(move)), 
+    omap(crane),
+    omap((moved) => pipe(modifyAt(to, concat(moved))(acc), getOrElse(() => acc))),
+    omap((changed) => pipe(modifyAt(from, dropRight(move))(changed), getOrElse(() => acc))),
+    getOrElse(() => acc)
+  )),
+  map(takeRight(1)),
+  flatten,
+  join('')
+);
 
-log(solve(parseStack(example))(parseMoves(example)))();
-//assert(2 === flow(lines, solution)(example));
-//assert(4 === flow(lines, solution))(example));
+assert('CMZ' === solve(parseStack(example), reverse)(parseMoves(example)));
+assert('MCD' === solve(parseStack(example), identity)(parseMoves(example)));
 
-//log("Solution day 5, part 1: " + flow(lines, solution(overlap))(file))();
-//log("Solution day 5, part 2: " + flow(lines, solution(nooverlap))(file))();
+
+log("Solution day 5, part 1: " + solve(parseStack(file), reverse)(parseMoves(file)))();
+log("Solution day 5, part 2: " + solve(parseStack(file), identity)(parseMoves(file)))();

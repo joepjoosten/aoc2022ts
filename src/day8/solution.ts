c4e38e61f5b2e6985bbfc06f24813febe81de767
Joep Joosten <joep.joosten@ihomer.nl>
2022-12-15 20:54:00 +0100

    WIP

--- src/day8/solution.ts
+++ src/day8/solution.ts
@@ -4,8 +4,9 @@ import { countBy, maximum, minimum, transpose } from "fp-ts-std/ReadonlyArray";
 import { lines } from "fp-ts-std/String";
 import { flip, flow, pipe } from "fp-ts/function";
 import { log } from "fp-ts/lib/Console";
+import { fst } from "fp-ts/lib/ReadonlyTuple";
 import * as N from "fp-ts/number";
-import { dropRight, filter, flap, flatten, map, matchLeft, reduce, reverse, size, zip, zipWith } from "fp-ts/ReadonlyArray";
+import { dropRight, filter, flap, flatten, map, mapWithIndex, matchLeft, reduce, reverse, size, splitAt, zip, zipWith } from "fp-ts/ReadonlyArray";
 import * as S from "fp-ts/string";
 import { readFileSync } from "fs";
 
@@ -19,25 +20,20 @@ const flipFlap = flip(flap)
 
 const parse = flow(lines, map(flow(chars, map(Number))));
 
-const lineOfSightMax: (xs: readonly number[]) => readonly number[] = flow(reduce<number, readonly number[]>([], (acc, x) => size(acc) === 0 ? [-1, x] : [...acc, maximum(N.Ord)([x, ...acc])]), dropRight(1));
+const viewFrom = (xs) => mapWithIndex((i, _) => ([pipe(splitAt(i)(xs)[0], reverse), splitAt(i + 1)(xs)[1]]))(xs)
 
-const fromLeft = (f) => map(f);
-const fromRight = (f) => flow(wrap(map(reverse))(map(f)))
-const fromUp = (f) => flow(wrap(transpose)(map(f)))
-const fromDown = (f) => flow(wrap(transpose)(wrap(map(reverse))(map(f))))
+pipe(example, parse, traceWithValue('input'), map(viewFrom), fst, traceWithValue("viewFrom"));
 
 const solve = (input) => pipe(
-    input,
-    flipFlap(flap(lineOfSightMax)([fromLeft, fromRight, fromUp, fromDown])), 
-    ([x, ...xs]) => reduce(x, flipZipWith(flipZipWith(Math.min)))(xs),
-    (xs) => flipZipWith(zip)(input, xs),
-    flatten,
-    filter(([x, y]) => x > y),
-    size
+    [input, transpose(input)],
+    flap(map(viewFrom)),
+    traceWithValue("viewFrom"),
 );
 
-assert(21 === solve(parse(example)));
+solve(parse(example));
+
+//assert(21 === solve(parse(example)));
 //assert(getEq(N.Eq).equals(some(29), solve(14)(example)));
 
-log("Solution day 8, part 1: " + solve(parse(file)))();
+//log("Solution day 8, part 1: " + solve(parse(file)))();
 //log("Solution day 8, part 2: " + pipe(solve(14)(file), getOrElse(() => -1)))();
